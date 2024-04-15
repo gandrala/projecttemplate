ff62432912359e38bf2e9279b6383d8abdd2eaac
gandrala <gandrala@gmail.com>
2024-04-15 17:50:01 +1000

    using env file for variables

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -1,2 +1,12 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+
 print("First dataset loaded")
 print("Second dataset about to load")
+
+dotenv_path = find_dotenv()
+print(dotenv_path)
+load_dotenv(dotenv_path)
+
+print(os.getenv("api_key"))
+print(os.getenv("password"))
